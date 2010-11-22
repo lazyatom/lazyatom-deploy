dc1f2ad7f0770c8a660706e121b0c694015a1164
James Adam <james@lazyatom.com>
2010-11-22 17:24:08 +0000

    We can avoid the freerange user by using the lower-level puppet functions.

--- puppet/lazyatom/manifests/init.pp
+++ puppet/lazyatom/manifests/init.pp
@@ -1,8 +1,18 @@
 class lazyatom {
-  class users {
-    include freerange
+  define user($user, $key, $key_type="ssh-rsa") {
+    freerange::user_without_ssh_key { $name:
+      user => $user,
+      shell => "/bin/zsh"
+    }
+    freerange::append_ssh_key_to_user { $name:
+      user => $user,
+      key => $key,
+      key_type => $key_type
+    }
+  }
 
-    freerange::user {"james@lazyatom.com":
+  class users {
+    lazyatom::user {"james@lazyatom.com":
       user => "james",
       key => "AAAAB3NzaC1yc2EAAAABIwAAAQEA2hsOv1phLQ6OsNMSLWMoXmV2q4qcaOZy6EDsX7+89WX1BQ/iCRckdvwhCc+KTYbIcCZA91JFhXtVwQ3tKDrJbdSYwTg3Y2a4dakbK87H63s7z5oIuM1KWXHGRV4RQGwqIgxNqkEwRUdbYkZ+Ct8X2W7bLTBkLA3zhOT7etxyg8WXRjGUdP6rpnAoJzVbnrn36SDKkWs+6yp5aNDhU4929En/8logAgBD3ByMAg9f/ndbVNA34ZuLWp6LYo2lZDrNxuAE4caseht+ovBWGiq84MmIMpVMuF0HuNNSh24CA8cF2DB7r+vYcmjHBz4VY7u4wkXBsPIbOjVwXVls5F7Hnw=="
     }
